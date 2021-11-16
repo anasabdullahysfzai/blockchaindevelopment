e5df7de828d940a0dc387b1388f38b9290240bb9
Anas Abdullah <anasabdullah014@gmail.com>
2021-11-17 01:29:48 +0500

    Mine Blocks

--- block.test.js
+++ block.test.js
@@ -35,3 +35,35 @@ describe("Genesis", ()=>{
     })
 
 });
+
+describe("mineBlock()",()=>{
+
+    const lastBlock = Block.genesis();
+    const data = "mined data"
+    const minedBlock = Block.mineBlock({lastBlock,data});
+
+    it("returns a Block Instance",()=>{
+
+        expect(minedBlock instanceof Block).toBe(true);
+
+    });
+
+    it("sets the `lastHash` to be the `hash` of `lastBlock`",()=>{
+
+        expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+
+    });
+
+    it("sets the `data`",()=>{
+
+        expect(minedBlock.data).toEqual(data);
+
+    })
+
+    it("sets the `timestamp`",()=>{
+
+        expect(minedBlock.timestamp).not.toEqual(undefined);
+
+    })
+
+})

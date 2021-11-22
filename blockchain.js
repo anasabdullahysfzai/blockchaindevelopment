96c17fdc33e0b242ec45e943efbb2936755fc2d2
Anas Abdullah <anasabdullah014@gmail.com>
2021-11-23 03:02:42 +0500

    Chain Validation Code and Tests

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 class Blockchain {
 
@@ -12,6 +13,47 @@ class Blockchain {
     {
         let newBlock = Block.mineBlock({lastBlock:this.chain[this.chain.length-1],data});
         this.chain.push(newBlock);
+    }
+
+    static isValidChain(chain)
+    {
+        //When chain doesnt start with genesis block
+        if(JSON.stringify(Block.genesis()) != JSON.stringify(chain[0]))
+        {
+            console.log(`Chain doesnt start with genesis block`)
+            return false;
+        }
+
+        //When last hash reference has changed
+        for(let i = 1 ; i<chain.length;i++)
+        {
+            if( chain[i].lastHash !== chain[i-1].hash)
+            {
+                console.log("lastHash reference changed")
+                return false;
+            }
+        }
+
+        //When block has invalid field
+        for(let i = 1; i<chain.length ; i++)
+        {
+            let {timestamp,data,lastHash,hash} = chain[i];
+
+            console.log(`This block = \n data:${data} \n lastHash: ${lastHash} \n timestamp: ${timestamp}`);
+            
+            let newHash = cryptoHash(timestamp,lastHash,data);
+
+            //If the new calculated hash doesnt match the actual hash then it means that some data has been changed ,
+            //so return false
+            if(newHash !== hash)
+            {
+                console.log("Invalid Hash");
+
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
--- blockchain.test.js
+++ blockchain.test.js
@@ -1,9 +1,21 @@
 const Blockchain = require("./blockchain");
 const Block = require("./block");
 
+let blockchain;
+
 describe("Blockchain",()=>{
 
-    const blockchain = new Blockchain();
+    
+
+    beforeEach(()=>{
+       
+        //Initiate Blockchain before running each test
+
+        blockchain = new Blockchain();
+
+        console.log("Initiated new Blockchain Instance");
+
+    })
 
     it("contins a `chain` array instance",()=>{
 
@@ -25,5 +37,76 @@ describe("Blockchain",()=>{
         expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
 
     })
+
+    describe("isValidChain()",()=>{
+
+        describe("when the chain doesnt start with genesis block",() => {
+
+            
+
+            it("returns false",()=>{
+
+                blockchain.chain[0] = { data: 'fake-genesis' }
+
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+
+            });
+
+        });
+
+        describe("when the chain start with genesis block and has multiple blocks",() => {
+
+            describe("and the `lastHash` reference has changed",()=>{
+
+                it("returns false",()=>{
+
+                    blockchain.addBlock({data: "Bears"});
+                    blockchain.addBlock({data: "Beets"});
+                    blockchain.addBlock({data: "Battlestar Galactica"});
+
+                    //Tamper the chain
+                    blockchain.chain[2].lastHash = "broken-lastHash";
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+
+                });
+            
+            });
+
+            describe("and the chain contains a block with invalid field",()=>{
+
+                it("returns false",()=>{
+
+                    blockchain.addBlock({data: "Bears"});
+                    blockchain.addBlock({data: "Beets"});
+                    blockchain.addBlock({data: "Battlestar Galactica"});
+
+                    //Change a field
+
+                    blockchain.chain[2].data = 'some-bad-and-evil-data';
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+
+                })
+
+            })
+
+            describe("and the `chain` doesnt contain any invalid block",()=>{
+
+                it("returns true ",()=>{
+
+                    blockchain.addBlock({data: "Bears"});
+                    blockchain.addBlock({data: "Beets"});
+                    blockchain.addBlock({data: "Battlestar Galactica"});
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+
+                });
+            
+            });
+
+        });
+
+    })   
 
 })

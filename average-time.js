0d5355c67a0818626dcdaaf7deb35c21dc1a3f39
Anas Abdullah <anasabdullah014@gmail.com>
2021-11-29 01:25:58 +0500

    Improving Proof of work ( Average Time Script )

---
+++ average-time.js
@@ -0,0 +1,30 @@
+let Blockchain = require('./blockchain.js');
+
+let blockchain = new Blockchain();
+
+let prevTime , newTime , nextBlock , averageTime;
+
+let timeTaken = [];
+
+blockchain.addBlock({data: "Test"});
+
+for (let i = 0;i<10000;i++)
+{
+
+    prevTime = blockchain.chain[blockchain.chain.length-1].timestamp;
+
+    blockchain.addBlock({data:`New Block : ${i+1}`});
+    
+    nextBlock = blockchain.chain[blockchain.chain.length-1];
+
+    newTime = nextBlock.timestamp;
+
+    let timeTakenToMine = newTime - prevTime;
+
+    timeTaken.push(timeTakenToMine);
+
+    averageTime = (timeTaken.reduce((total,item)=> total + item))/timeTaken.length;
+
+    console.log(`Time to mine new block: ${timeTakenToMine}ms. Difficulty: ${nextBlock.difficulty}. Average Time: ${averageTime}ms.`);
+
+}

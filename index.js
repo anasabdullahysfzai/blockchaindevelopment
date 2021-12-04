9ddc728e93ac9819d867c3cc3f26c6723787c384
Anas Abdullah <anasabdullah014@gmail.com>
2021-12-05 00:15:25 +0500

    Blockchain boardcasting with redis and redis cloud

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const Blockchain = require('./blockchain.js');
+const PubSub = require('./pubsub.js');
 const app = express();
 
 const blockchain = new Blockchain();
+const pubsub = new PubSub({blockchain});
 
-const PORT = 3000;
+setTimeout(() => {pubsub.broadcastBlockchain()},1000)
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
@@ -21,12 +25,24 @@ app.post("/api/mine",(req,res)=>{
 
     blockchain.addBlock({data});
 
+    pubsub.broadcastBlockchain();
+
     res.redirect(303,"/api/blocks");
 
 })
 
 // app.post('/api/',(req,res)=>{})
 
+const DEFAULT_PORT = 3000;
+
+let PEER_PORT;
+
+if(process.env.GENERATE_PEER_PORT === "true") 
+{
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT,()=>{
 
---
+++ pubsub.js
@@ -0,0 +1,62 @@
+const redis = require("redis");
+
+const CHANNELS = {
+    TEST: "TEST",
+    BLOCKCHAIN: "BLOCKCHAIN"
+}
+
+const redisUrl = "//redis-11774.c265.us-east-1-2.ec2.cloud.redislabs.com:11774";
+
+const redisConf = {password: "redisdb123"}
+
+class PubSub {
+
+    constructor({blockchain}) {
+
+        this.blockchain = blockchain;
+
+        this.publisher = new redis.createClient(redisUrl,redisConf);
+        this.subscriber = new redis.createClient(redisUrl,redisConf);
+
+        this.subscribeToChannels();
+
+        this.subscriber.on("message", (channel,message)=>{
+            this.handleMessage(channel, message);
+        })
+
+    }
+
+
+    handleMessage(channel, message)
+    {
+        console.log(`Message Received. Channel. ${channel}. Message: ${message}.`);
+
+        //Parse JSON
+        const newchain = JSON.parse(message);
+
+        if(channel === CHANNELS.BLOCKCHAIN)
+        {
+            this.blockchain.replaceChain(newchain);
+        }
+
+    }
+
+    subscribeToChannels()
+    {
+        Object.values(CHANNELS).forEach((channel,index,array)=>{
+            this.subscriber.subscribe(channel);
+        })
+    }
+
+    publish({channel,message})
+    {
+        this.publisher.publish(channel,message);
+    }
+
+    broadcastBlockchain()
+    {
+        this.publish({channel:CHANNELS.BLOCKCHAIN,message: JSON.stringify(this.blockchain.chain)});
+    }
+}
+
+module.exports = PubSub;

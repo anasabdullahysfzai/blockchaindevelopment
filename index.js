26597251a053e0dfe58cb2b37b9304b6ae77a314
Anas Abdullah <anasabdullah014@gmail.com>
2021-12-06 02:07:01 +0500

    Sync chain on connection

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const express = require('express');
+const app = express();
 const Blockchain = require('./blockchain.js');
 const PubSub = require('./pubsub.js');
-const app = express();
+const axios = require("axios").default;
+
 
 const blockchain = new Blockchain();
-const pubsub = new PubSub({blockchain});
+const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => {pubsub.broadcastBlockchain()},1000)
+setTimeout(() => { pubsub.broadcastBlockchain() }, 1000)
 
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/blocks',(req,res)=>{
+app.get('/api/blocks', (req, res) => {
 
     res.status(200).json(blockchain.chain);
 
 });
 
-app.post("/api/mine",(req,res)=>{
+app.post("/api/mine", (req, res) => {
 
-    let {data} = req.body;
+    let { data } = req.body;
 
-    blockchain.addBlock({data});
+    blockchain.addBlock({ data });
 
     pubsub.broadcastBlockchain();
 
-    res.redirect(303,"/api/blocks");
+    res.redirect(303, "/api/blocks");
 
 })
 
@@ -37,15 +39,37 @@ const DEFAULT_PORT = 3000;
 
 let PEER_PORT;
 
-if(process.env.GENERATE_PEER_PORT === "true") 
-{
-    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000);
+if (process.env.GENERATE_PEER_PORT === "true") {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
 const PORT = PEER_PORT || DEFAULT_PORT;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
 
     console.log(`Server started on ${PORT}`);
 
+    //sync the chain with root node of network
+
+    const ROOT_NODE_URL = `http://localhost:${DEFAULT_PORT}`;
+
+
+    let isRootNode = `http://localhost:${PORT}` === ROOT_NODE_URL;
+
+    //If the recently created blockchain network node is not root node then update it with latest chain data
+    if (!isRootNode) {
+        axios.get(ROOT_NODE_URL + "/api/blocks")
+            .then((response) => {
+
+                const latestChain = response.data;
+                console.log(`latestChain: ${JSON.stringify(latestChain)}`);
+
+                blockchain.replaceChain(latestChain);
+
+                console.log("Blockchain updated with latest chain from root node \n "+ JSON.stringify(blockchain.chain));
+
+            }).catch((error) => {
+                console.log(error);
+            });
+    }
 });
